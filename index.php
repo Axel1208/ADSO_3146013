92338ae5c88037226c5c42644f3f8f887d8251c6
Axel1208 <juandavidospinagutierrez44gmail.com>
2025-11-12 11:01:54 -0500

    12 noviembre, insert tabla usuarios

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+require "controller.php";
+
+$controller = new Controller;
+
+$controller->index();
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    $controller->guardar();
+}
---
+++ view.php
@@ -0,0 +1,13 @@
+
+<form action="" method="post">
+    <input type="text" name='nombre'
+    placeholder="nombre">
+    <input type="email" name="correo" 
+    placeholder="correo">
+    <input type="text" name="rol" placeholder="rol">
+    <input type="text" name="telefono"
+    placeholder="telefono">
+
+    
+    <input type="submit" value="guardar">
+</form>
